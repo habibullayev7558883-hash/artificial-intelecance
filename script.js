929fb56857683bec6e9d33ef49d72fba92e1397e
habibullayev7558883@gmail.com <habibullayev7558883@gmail.com>
2026-01-02 06:51:20 +0500

    add code in js that promt

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const elButton = document.querySelector(".btn");
 elButton.addEventListener("click",()=>{
 document.body.classList.toggle("dark"); });
+
+let ism = prompt("Assalomu laykum siz Otabek akani saytiga kirmoqdasiz!! Iltimos ismingizni qoldiring!!!")
+console.log(ism);
